7696706b18dcc469ad42385e98b2c6fb58afcc40
Hildebrandt-Alex <alex.hildebrandt@tum.de>
2020-05-12 15:20:49 +0200

    first post

--- get_started.R
+++ get_started.R
@@ -17,8 +17,9 @@ blogdown::new_site(theme = "yoshiharuyamashita/blackburn")
 
 # managing changes with git and RStudio
 # https://r-bio.github.io/intro-git-rstudio/
-
+# https://happygitwithr.com/
 # add files of the repository to git with commands from terminal
 #1. git add --all
-#2. git status       --> check if they have added -> will be green marked
-#3. 
+#2. git status                    --> check if they have added -> will be green marked
+#3. git commit -m "first commit"  --> email-adress and name
+#4. git push                      -->push files to Github
